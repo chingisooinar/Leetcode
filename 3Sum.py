3a2fbb36b6edbccd69256c0d28df33e40336b019
Chingis Oinar <chingisoinar@gmail.com>
2021-01-19 07:04:46 +0900

    Runtime: 976 ms, faster than 49.83% of Python3 online submissions for 3Sum.

--- 3Sum.py
+++ 3Sum.py
@@ -1,10 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        def check_if_no_answer(sorted_int_list):
+                        # check there can be extra answers in the sorted list
+            if len(sorted_int_list) < 3:
+                return True
+
+            if sorted_int_list[0] > 0:
+                return True
+
+            if sorted_int_list[-1] < 0:
+                return True
+            return False
+    
         nums.sort()
         tagret = 0
         out = []
         prev = 0
         for i in range(len(nums)):
+            if check_if_no_answer(nums[i:]):
+                return out
             target = 0-nums[i]
             if i>0 and target == prev:
                 continue
