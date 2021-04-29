af9683ef27661f78c80ed277a92e8f265ca475bd
Chingis Oinar <chingisoinar@gmail.com>
2021-04-29 18:17:57 +0900

    Update intToRoman.py

--- intToRoman.py
+++ intToRoman.py
@@ -2,33 +2,32 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         nums = []
         d = 10
+        res = []
+        syms = ['I','V','X','L','C','D','M']
+        vals = [1,5,10,50,100,500,1000]
         while num!=0:
             n = num%d
             if n != 0:
-                nums.append(n)
+                i = 0
+                while i < len(vals) and vals[i] <= n:
+                    i += 1
+                i -= 1
+                if vals[i] == n:
+                    res.append(syms[i])
+                elif i + 1 < len(vals) and vals[i + 1] - n in [1, 10, 100]:
+                    res.append(syms[vals.index(vals[i + 1] - n)] +  syms[i + 1])
+                else:
+                    tmp=''
+                    while n > 0:
+                        n -= vals[i]
+                        tmp += syms[i]
+                        if vals[i] > n:
+                            i -= 1
+                    res.append(tmp)
             num = num // d * d
             d *= 10
-        res = ''
-        syms = ['I','V','X','L','C','D','M']
-        vals = [1,5,10,50,100,500,1000]
-        while nums:
-            n = nums.pop()
-            i = 0
-            while i < len(vals) and vals[i] <= n:
-                i += 1
-            i -= 1
-            if vals[i] == n:
-                res += syms[i]
-            elif i + 1 < len(vals) and vals[i + 1] - n in [1, 10, 100]:
-                res += syms[vals.index(vals[i + 1] - n)]
-                res += syms[i + 1]
-            else:
-                while n > 0:
-                    n -= vals[i]
-                    res += syms[i]
-                    if vals[i] > n:
-                        i -= 1
-        return res
+        res.reverse()
+        return ''.join(res)
                 
             
             
