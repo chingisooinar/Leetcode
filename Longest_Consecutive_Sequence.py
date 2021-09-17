c319e6eaa9ad20ba0479f2f66f29fbe2cf85ee7d
Chingis Oinar <chingisoinar@gmail.com>
2021-09-17 12:02:37 +0900

    Memory Usage: 25.7 MB, less than 84.80% of Python3 online submissions for Longest Consecutive Sequence. O(n) solution
    Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
    
    You must write an algorithm that runs in O(n) time.

---
+++ Longest_Consecutive_Sequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        memo = set(nums)
+        longest = 0
+        
+        for num in nums:
+            if num - 1 not in memo:
+                cur = num
+                count = 1
+                while cur + 1 in memo:
+                    cur = cur + 1
+                    count += 1
+                longest = max(longest, count)
+                
+        return longest
+        
